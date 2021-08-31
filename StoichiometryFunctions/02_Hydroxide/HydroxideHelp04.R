b1b49cd3954bc7feb1d233f93cfc1733c5a62d10
david.elias949@gmail.com <david.elias949@gmail.com>
2021-08-31 11:29:02 -0300

    HydroxideHelp04() - Spanish - Ready!

---
+++ StoichiometryFunctions/02_Hydroxide/HydroxideHelp04.R
@@ -0,0 +1,208 @@
+
+
+# Hydroxide Helper
+HydroxideHelp04 <-function(input_atomic_number1 = NULL, input_valence1 = NULL, 
+                      input_internal_language = "en",
+                      input_external_language = NULL,
+                      input_PeriodicTable = NULL,
+                      input_family = NULL,
+                      input_Helper = NULL) {
+  
+ 
+  # Internal Settings
+  {
+    ###
+    
+    # Important Detail!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Internally are using "en" (English) as internal language and options.
+    # The input_optional_language is for select some correct options form each language
+    # in the output.
+    input_internal_language <- "en"
+    if (is.null(input_external_language)) input_external_language <- "en"
+    
+    ###  
+  } # End Internal Settings
+  ##########################################################################
+  
+  
+  
+  # Part 0: Necessary Participants
+  {
+    ###
+    
+    # We gather all the information we need to
+    # carry out the exercise
+    
+    # Principal objects
+    atomic_number1 <- as.numeric(as.character(input_atomic_number1))
+    valence1 <-  as.numeric(as.character(input_valence1))
+    internal_language <- as.character(input_internal_language)
+    
+    # Recruited values
+    symbol1 <- as.character(input_PeriodicTable[[internal_language]][atomic_number1, 2])
+    name1  <- as.character(input_PeriodicTable[[internal_language]][atomic_number1, 3])
+    type1 <- as.character(input_PeriodicTable[[internal_language]][atomic_number1, 7])
+    state1 <- as.character(input_PeriodicTable[[internal_language]][atomic_number1, 9])
+    
+    # Internal Control 
+    internal_control <- InternalControl_Hydroxide(input_atomic_number1 = input_atomic_number1,
+                                              input_valence1 = input_valence1,
+                                              input_internal_language = input_internal_language,
+                                              input_external_language = input_external_language,
+                                              input_PeriodicTable = input_PeriodicTable)
+    
+    
+    input_hydroxide_resolution <- HydroxideEquation(input_atomic_number1 = input_atomic_number1,
+                                             input_valence1 = input_valence1,
+                                             input_internal_language = "en",
+                                             input_external_language = input_external_language,
+                                             input_PeriodicTable = input_PeriodicTable)
+    
+    ###  
+  } # End Part 0: Necessary Participants
+  ####################################################
+
+  
+  # Part 1: Isolate 
+  {
+  ###
+    
+    the_helper04 <- input_Helper[[input_family]][[input_external_language]]
+    selected_columns <- c((ncol(the_helper04)-1), ncol(the_helper04))
+    the_helper04 <- the_helper04[,selected_columns]
+    
+    # Options
+    my_YES <- the_helper04[,1]
+    my_NO <- the_helper04[,2]
+    
+    # General substitution
+    my_YES <- gsub("_element1_", symbol1, my_YES)
+    my_YES <- gsub("_valence1_", valence1, my_YES)
+    
+    my_NO <- gsub("_element1_", symbol1, my_NO)
+    my_NO <- gsub("_valence1_", valence1, my_NO)
+    
+    # Default Values is NO
+    new_help04 <- my_NO
+    
+  ###  
+  } # End Part1
+  ############################
+  
+  
+  # Part 2: Selection
+  {
+  ###
+    
+    # Step 1: Default value
+   
+    # Step 2: Default value
+    
+    # Step 3: Default value
+    
+    # Step 4: New coefficient 3?
+    {
+      ###
+      my_3 <- as.numeric(as.character(as.vector(input_hydroxide_resolution[3, c("Coef3")])))
+      my_4 <- as.numeric(as.character(as.vector(input_hydroxide_resolution[4, c("Coef3")])))
+      
+      if (!identical(my_3, my_4)){ 
+
+        new_help04[4] <- my_YES[4] 
+        new_help04[4] <- gsub("_coef3_", my_4, new_help04[4])
+
+      }
+      
+      remove(my_3, my_4)
+      ###  
+    } # End Step 5
+    #######################################
+    
+    # Step 5: New coefficient 2?
+    {
+      ###
+      my_4 <- as.numeric(as.character(as.vector(input_hydroxide_resolution[4, c("Coef2")])))
+      my_5 <- as.numeric(as.character(as.vector(input_hydroxide_resolution[5, c("Coef2")])))
+      
+      if (!identical(my_4, my_5)){ 
+        
+        new_help04[5] <- my_YES[5] 
+        new_help04[5] <- gsub("_coef2_", my_5, new_help04[5])
+        
+      }
+      
+      remove(my_4, my_5)
+      ###  
+    } # End Step 5
+    #######################################
+    
+    # Step 6: Default value
+    
+    # Step 7: Coefficients simplificated?
+    {
+      ###
+      my_6 <- as.numeric(as.character(as.vector(input_hydroxide_resolution[6, c("Coef1", "Coef2", "Coef3")])))
+      my_7 <- as.numeric(as.character(as.vector(input_hydroxide_resolution[7, c("Coef1", "Coef2", "Coef3")])))
+      
+      if (!identical(my_6, my_7)){ 
+        library(numbers)
+        mcd_6 <- mGCD(my_6)
+        new_help04[7] <- my_YES[7] 
+        new_help04[7] <- gsub("_mcd_coef_", mcd_6, new_help04[7])
+        
+        remove(mcd_6)
+      }
+      
+      remove(my_6, my_7)
+      ###  
+    } # End Step 7
+    #######################################
+    
+    
+    
+ 
+    
+  ###  
+  } # End Part2
+  ##################################
+  
+  
+  
+  
+      # Part 3: Return
+      {
+      ###
+        return(new_help04)
+        
+      ###  
+      } # End Part 3: Exit
+      #######################################
+      
+  
+                 
+
+}  
+
+
+
+# 
+# input_atomic_number1 <- 26
+# input_valence1 <- 2
+# input_internal_language <- "en"
+# input_external_language <- "es"
+# input_PeriodicTable <- PeriodicTable
+# input_family = "Hydroxide"
+# input_Helper = PageHelperLevel
+# 
+# 
+# 
+# input_hydroxide_help04 <- HydroxideHelp04(input_atomic_number1 = input_atomic_number1,
+#                                         input_valence1 = input_valence1,
+#                                         input_internal_language = "en",
+#                                         input_external_language = input_external_language,
+#                                         input_PeriodicTable = input_PeriodicTable,
+#                                         input_family = input_family,
+#                                         input_Helper = input_Helper)
+# 
+# 
+# input_hydroxide_help04
--- StoichiometryFunctions/08_General/GeneralHelp04.R
+++ StoichiometryFunctions/08_General/GeneralHelp04.R
@@ -38,67 +38,74 @@ GeneralHelp04 <- function(input_atomic_number1 = NULL,
       if ("Hydroxide" == input_family) {
         
         # Hydroxide!
-        general_help04 <- HydroxideLaTeX(input_atomic_number1 = input_atomic_number1,
-                                                input_valence1 = input_valence1,
-                                                input_internal_language = "en",
-                                                input_external_language = input_external_language,
-                                                input_PeriodicTable = input_PeriodicTable)
+        general_help04 <- HydroxideHelp04(input_atomic_number1 = input_atomic_number1,
+                                          input_valence1 = input_valence1,
+                                          input_internal_language = "en",
+                                          input_external_language = input_external_language,
+                                          input_PeriodicTable = input_PeriodicTable,
+                                          input_family = input_family,
+                                          input_Helper = input_Helper)
       } else
         if ("Oxacid" == input_family) {
           
           # Oxacid
-          general_help04 <- OxacidLaTeX(input_atomic_number1 = input_atomic_number1,
-                                               input_valence1 = input_valence1,
-                                               input_internal_language = "en",
-                                               input_external_language = input_external_language,
-                                               input_PeriodicTable = input_PeriodicTable)
+          general_help04 <- OxacidHelp04(input_atomic_number1 = input_atomic_number1,
+                                        input_valence1 = input_valence1,
+                                        input_internal_language = "en",
+                                        input_external_language = input_external_language,
+                                        input_PeriodicTable = input_PeriodicTable,
+                                        input_family = input_family,
+                                        input_Helper = input_Helper)
           
         } else
           if ("Hydride" == input_family) {
             
             # Hydride
-            general_help04 <- HydrideLaTeX(input_atomic_number1 = input_atomic_number1,
-                                                  input_valence1 = input_valence1,
-                                                  input_internal_language = "en",
-                                                  input_external_language = input_external_language,
-                                                  input_PeriodicTable = input_PeriodicTable)
+            general_help04 <- HydrideHelp04(input_atomic_number1 = input_atomic_number1,
+                                           input_valence1 = input_valence1,
+                                           input_internal_language = "en",
+                                           input_external_language = input_external_language,
+                                           input_PeriodicTable = input_PeriodicTable,
+                                           input_family = input_family,
+                                           input_Helper = input_Helper)
           } else
             if ("Hydracid" == input_family) {
               
               # Hydradic
-              general_help04 <- HydracidLaTeX(input_atomic_number1 = input_atomic_number1,
-                                                     input_valence1 = input_valence1,
-                                                     input_internal_language = "en",
-                                                     input_external_language = input_external_language,
-                                                     input_PeriodicTable = input_PeriodicTable)
+              general_help04 <- HydracidHelp04(input_atomic_number1 = input_atomic_number1,
+                                              input_valence1 = input_valence1,
+                                              input_internal_language = "en",
+                                              input_external_language = input_external_language,
+                                              input_PeriodicTable = input_PeriodicTable,
+                                              input_family = input_family,
+                                              input_Helper = input_Helper)
             } else
               if ("Oxosalt" == input_family) {
                 
                 # Oxosalt
-                general_help04 <- OxosaltLaTeX(input_atomic_number1 = input_atomic_number1,
-                                                      input_valence1 = input_valence1,
-                                                      input_atomic_number2 = input_atomic_number2,
-                                                      input_valence2 = input_valence2,
-                                                      input_internal_language = "en",
-                                                      input_external_language = input_external_language,
-                                                      input_PeriodicTable = input_PeriodicTable)
+                general_help04 <- OxosaltHelp04(input_atomic_number1 = input_atomic_number1,
+                                               input_valence1 = input_valence1,
+                                               input_atomic_number2 = input_atomic_number2,
+                                               input_valence2 = input_valence2,
+                                               input_internal_language = "en",
+                                               input_external_language = input_external_language,
+                                               input_PeriodicTable = input_PeriodicTable,
+                                               input_family = input_family,
+                                               input_Helper = input_Helper)
               } else
                 if ("Salt" == input_family) {
                   
                   # Salt
-                  general_help04 <- SaltLaTeX(input_atomic_number1 = input_atomic_number1,
-                                                     input_valence1 = input_valence1,
-                                                     input_atomic_number2 = input_atomic_number2,
-                                                     input_valence2 = input_valence2,
-                                                     input_internal_language = "en",
-                                                     input_external_language = input_external_language,
-                                                     input_PeriodicTable = input_PeriodicTable)
-                } else
-                  if(sum(my_family == input_family) == 0) {
-                    
-                    # Emergency!
-                    general_help04 <- matrix_emergency_exit
-                  }
+                  general_help04 <- SaltHelp04(input_atomic_number1 = input_atomic_number1,
+                                              input_valence1 = input_valence1,
+                                              input_atomic_number2 = input_atomic_number2,
+                                              input_valence2 = input_valence2,
+                                              input_internal_language = "en",
+                                              input_external_language = input_external_language,
+                                              input_PeriodicTable = input_PeriodicTable,
+                                              input_family = input_family,
+                                              input_Helper = input_Helper)
+                } 
     ###
   } # End Part 1: Structure of the "matrix_solution"
   ############################################################
